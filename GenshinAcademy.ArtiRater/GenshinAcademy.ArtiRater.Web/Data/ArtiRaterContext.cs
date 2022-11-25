6f91d3f0d28a1487e195338dd1d35a6f0ce60a9a
Order <43047529+aelariane@users.noreply.github.com>
2022-11-25 23:03:01 +0300

    Added database context

---
+++ GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/Data/ArtiRaterContext.cs
@@ -0,0 +1,32 @@
+﻿using GenshinAcademy.ArtiRater.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenshinAcademy.ArtiRater.Web.Data
+{
+    public class ArtiRaterContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<ArtifactStatsProfit> Profits { get; set; }
+
+        public ArtiRaterContext(DbContextOptions<ArtiRaterContext> config) : base(config)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Character>()
+                .HasMany(x => x.StatsProfit)
+                .WithOne(y => y.OwnerCharacter)
+                .HasForeignKey(y => y.OwnerCharacterId);
+
+            modelBuilder.Entity<ArtifactStatsProfit>()
+                .Property(y => y.Type)
+                .HasConversion<int>();
+        }
+    }
+}
