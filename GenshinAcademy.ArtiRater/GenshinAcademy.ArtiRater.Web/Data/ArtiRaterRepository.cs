c3a1f89e1196a2fb9f73f1fa7844dcccac6db00a
Order <43047529+aelariane@users.noreply.github.com>
2022-11-25 23:05:51 +0300

    Create ArtiRaterRepository.cs

---
+++ GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/Data/ArtiRaterRepository.cs
@@ -0,0 +1,31 @@
+﻿using GenshinAcademy.ArtiRater.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenshinAcademy.ArtiRater.Web.Data
+{
+    public class ArtiRaterRepository
+    {
+        private ArtiRaterContext _context;
+
+        public ArtiRaterRepository(ArtiRaterContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Character>> GetCharacters()
+        {
+            var result = await _context.Characters
+                .ToArrayAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<Character>> GetCharactersWithProfits()
+        {
+            var result = await _context.Characters
+                .Include(x => x.StatsProfit)
+                .ToArrayAsync();
+
+            return result;
+        }
+    }
+}
