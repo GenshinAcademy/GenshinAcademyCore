9df3b53093e19b80c70dd7d80f304abf483fdd44
Order <43047529+aelariane@users.noreply.github.com>
2022-11-26 07:33:35 +0300

    Update CharacterWithProfitsModel.cs

--- GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/WebModels/CharacterWithProfitsModel.cs
+++ GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/WebModels/CharacterWithProfitsModel.cs
@@ -1,5 +1,6 @@
 ﻿using GenshinAcademy.ArtiRater.Web.Models;
 using GenshinAcademy.ArtiRater.Web.Models.Enums;
+using GenshinAcademy.ArtiRater.Web.WebModels.ArtifactProfits;
 using System.Text.Json.Serialization;
 
 namespace GenshinAcademy.ArtiRater.Web.WebModels
@@ -8,24 +9,24 @@ namespace GenshinAcademy.ArtiRater.Web.WebModels
     {
 
         [JsonPropertyName("stats_profit")]
-        public IReadOnlyDictionary<string, object> StatsProft { get; set; }
+        public IReadOnlyDictionary<string, ArtifactProfitModel> StatsProft { get; set; }
 
         public CharacterWithProfitsModel(Character dataModel, IEnumerable<ArtifactStatsProfit> profits) : base(dataModel)
         {
             Name = dataModel.Name;
             Element = dataModel.Element;
             IconUrl = dataModel.IconUrl;
-            var dictionary = new Dictionary<string, object>(6);
+            var dictionary = new Dictionary<string, ArtifactProfitModel>(6);
             foreach (ArtifactStatsProfit profit in profits)
             {
                 ArtifactProfitModel model = profit.Type switch
                 {
-                    ArtifactStatProfitType.Circlet => new ArtifactProfits.CircletProfitModel(profit),
-                    ArtifactStatProfitType.Feather => new ArtifactProfits.FeatherProfitModel(profit),
-                    ArtifactStatProfitType.Flower => new ArtifactProfits.FlowerProfitModel(profit),
-                    ArtifactStatProfitType.Goblet => new ArtifactProfits.GobletProfitModel(profit),
-                    ArtifactStatProfitType.Sands => new ArtifactProfits.SandsProfitModel(profit),
-                    ArtifactStatProfitType.SubStats => new ArtifactProfits.SubstatsProfitModel(profit),
+                    ArtifactStatProfitType.Circlet  => new CircletProfitModel(profit),
+                    ArtifactStatProfitType.Feather  => new FeatherProfitModel(profit),
+                    ArtifactStatProfitType.Flower   => new FlowerProfitModel(profit),
+                    ArtifactStatProfitType.Goblet   => new GobletProfitModel(profit),
+                    ArtifactStatProfitType.Sands    => new SandsProfitModel(profit),
+                    ArtifactStatProfitType.SubStats => new SubstatsProfitModel(profit),
                     _ => throw new Exception($"Unknown type of profit type {((int)profit.Type)}")
                 };
                 dictionary.Add(model.Key, model);
