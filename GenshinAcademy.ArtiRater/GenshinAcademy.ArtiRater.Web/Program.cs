25ce90e16f5dc2c25607b395e8e41afefb9776cc
Order <43047529+aelariane@users.noreply.github.com>
2022-11-26 07:52:40 +0300

    Reworked Program to old asp.net core way

--- GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/Program.cs
+++ GenshinAcademy.ArtiRater/GenshinAcademy.ArtiRater.Web/Program.cs
@@ -1,31 +1,20 @@
 using GenshinAcademy.ArtiRater.Web.Data;
+using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GenshinAcademy.ArtiRater.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            string dbConnection = "";
-
-            builder.Services.AddControllers();
-            builder.Services.AddDbContext<ArtiRaterContext>(x =>
-            {
-                //x.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BookmarksOnline;Trusted_Connection=True;");
-                x.UseNpgsql(dbConnection);
-            });
-            var app = builder.Build();
-
-           // app.UseHttpsRedirection();
-
-            app.MapControllers();
-
-            app.Run();
+            IWebHost host = BuildWebHost(args);
+            await host.RunAsync();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
     }
 }
