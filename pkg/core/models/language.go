2362a239834afef734d6c21a1b141c6b8668c810
Order <43047529+aelariane@users.noreply.github.com>
2023-01-26 22:18:22 +0300

    Added create string from language

--- pkg/core/models/language.go
+++ pkg/core/models/language.go
@@ -1,6 +1,17 @@
 package models
 
+import "ga/pkg/core/value_objects/localized_string"
+
+// Language
 type Language struct {
 	BaseModel
 	LanguageName string
+}
+
+// Create new string for specified language
+func (lang Language) CreateNewString(value string) localized_string.LocalizedString {
+	if lang.Id == 0 {
+		panic("Cannot create new string for not initialized language")
+	}
+	return localized_string.New(localized_string.LanguageId(lang.Id), value)
 }
