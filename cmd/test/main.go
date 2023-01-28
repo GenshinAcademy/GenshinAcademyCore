424d8c060c4c902d111607a67a42381045528f63
Order <43047529+aelariane@users.noreply.github.com>
2023-01-28 11:31:47 +0300

    Updated test sample

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -5,7 +5,8 @@ import (
 	core "ga/pkg/core"
 	"ga/pkg/core/models"
 	"ga/pkg/core/repositories"
-	db "ga/pkg/db_postgres"
+	"ga/pkg/core/repositories/find_parameters"
+	"ga/pkg/core_postgres"
 )
 
 // Sample method for creating characters
@@ -14,19 +15,29 @@ func createHuTao(language models.Language, charRepo *repositories.ICharacterRepo
 	//localized_string.New(languageId, "SomeTitle"), Is valid option to craete strings too!
 	var hutao = models.Character{
 		CharacterId: "hu_tao",
-		Name:        language.CreateNewString("Hu Tao"),
-		FullName:    language.CreateNewString("Hu Taoooo"),
-		Description: language.CreateNewString("Pyro damage dealer"),
-		Title:       language.CreateNewString("SomeTitle"),
 		Element:     models.Pyro,
 		Rarity:      models.Legendary,
 		Weapon:      models.Polearm,
 	}
+
+	hutao.Name = language.CreateString(hutao.Name, "Hu Tao")
+	hutao.FullName = language.CreateString(hutao.FullName, "Hu Taoooo")
+	hutao.Description = language.CreateString(hutao.Description, "Pyro character")
+	hutao.Title = language.CreateString(hutao.Title, "Title")
+
 	(*charRepo).AddCharacter(&hutao)
 }
 
+func updateCharacter(language models.Language, character *models.Character, repo repositories.ICharacterRepository) {
+	character.Name = language.CreateString(character.Name, "Name"+language.LanguageName)
+	character.FullName = language.CreateString(character.FullName, "FullName"+language.LanguageName)
+	character.Description = language.CreateString(character.Description, "Description"+language.LanguageName)
+	character.Title = language.CreateString(character.Title, "Title"+language.LanguageName)
+	repo.UpdateCharacter(character)
+}
+
 func main() {
-	var dbConfig db.PostgresDatabaseConfiguration = db.PostgresDatabaseConfiguration{
+	var dbConfig core_postgres.PostgresDatabaseConfiguration = core_postgres.PostgresDatabaseConfiguration{
 		Host:         "localhost",
 		UserName:     "postgres",
 		UserPassword: "12345678",
@@ -34,15 +45,15 @@ func main() {
 		Port:         "5432",
 		ServerPort:   "",
 	}
-	db.InitializePostgresDatabase(dbConfig)
-	defer db.CleanupConnections()
+	core_postgres.InitializePostgresDatabase(dbConfig)
+	defer core_postgres.CleanupConnections()
 
-	var defaultLanguage = "English"
+	var defaultLanguage = "Russian"
 	//Initializing gacore config and configure it for postgres db
 	var config core.GenshinCoreConfiguration = core.GenshinCoreConfiguration{
 		DefaultLanguage: defaultLanguage,
 	}
-	db.ConfigurePostgresDB(&config)
+	core_postgres.ConfigurePostgresDB(&config)
 
 	//Create ga core
 	var gacore *core.GenshinCore = core.CreateGenshinCore(config)
@@ -51,12 +62,14 @@ func main() {
 
 	//Create language if it does not exist
 	var language = langRepo.FindLanguage(gacore.GetDefaultLanguageName())
+	var upd bool = false
 	if language.Id == 0 {
 		language = models.Language{
 			LanguageName: defaultLanguage,
 		}
 		langRepo.AddLanguage(&language)
 		fmt.Println("Language created successfully!\n", language)
+		upd = true
 	} else {
 		fmt.Println("Language found soccessfully!\n", language)
 	}
@@ -65,12 +78,18 @@ func main() {
 	var provider = gacore.GetDefaultProvider()
 	var characterRepo = provider.NewCharacterRepo()
 
-	var hutao = characterRepo.FindCharacterById(1)
-	if hutao.Id == 0 {
+	var findParams = find_parameters.FindByCharacterId("hu_tao")
+	var result = characterRepo.FindCharacters(findParams)
+	var hutao models.Character = characterRepo.FindCharacterById(1)
+	if len(result) == 0 {
 		createHuTao(language, &characterRepo)
-		var hutaoNew = characterRepo.FindCharacterById(1)
+		var hutaoNew = characterRepo.FindCharacters(findParams)[0]
 		fmt.Println("Hu tao successfully added to DB!\n", hutaoNew)
 		return
+	}
+	if upd {
+		updateCharacter(language, &hutao, characterRepo)
+		fmt.Println("Hu Tao model updated successfully!\n", hutao)
 	}
 
 	fmt.Println("Hu Tao model retrieved successfully!\n", hutao)
