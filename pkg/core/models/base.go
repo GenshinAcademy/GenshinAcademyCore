795ca7e7eaa9391ae31bbc2202569018df1b820b
Order <43047529+aelariane@users.noreply.github.com>
2023-01-28 09:51:37 +0300

    Added undefined id const and validity check

--- pkg/core/models/base.go
+++ pkg/core/models/base.go
@@ -2,6 +2,14 @@ package models
 
 type ModelId uint64
 
+const (
+	UNDEFINED_ID ModelId = 0
+)
+
 type BaseModel struct {
 	Id ModelId
+}
+
+func (id ModelId) IsValid() bool {
+	return id != UNDEFINED_ID
 }
