3a220a762bb6ccf946a4021023e192fab6f905eb
Order <43047529+aelariane@users.noreply.github.com>
2023-01-28 11:31:30 +0300

    Update language.go

--- pkg/core/models/language.go
+++ pkg/core/models/language.go
@@ -8,10 +8,23 @@ type Language struct {
 	LanguageName string
 }
 
-// Create new string for specified language
 func (lang Language) CreateNewString(value string) localized_string.LocalizedString {
 	if lang.Id == 0 {
 		panic("Cannot create new string for not initialized language")
 	}
+
 	return localized_string.New(localized_string.LanguageId(lang.Id), value)
+}
+
+// Create new string for specified language
+func (lang Language) CreateString(src localized_string.LocalizedString, value string) localized_string.LocalizedString {
+	if lang.Id == 0 {
+		panic("Cannot create new string for not initialized language")
+	}
+
+	if src.GetId() == 0 {
+		return localized_string.New(localized_string.LanguageId(lang.Id), value)
+	}
+
+	return localized_string.Create(src.GetId(), localized_string.LanguageId(lang.Id), value)
 }
