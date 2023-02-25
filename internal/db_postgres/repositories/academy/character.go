e55e35fb4777506e3283fb0a479e1c91f8c0e03e
Order <43047529+aelariane@users.noreply.github.com>
2023-02-25 08:17:14 +0300

    fix afer global rename

--- internal/db_postgres/repositories/academy/character.go
+++ internal/db_postgres/repositories/academy/character.go
@@ -39,7 +39,7 @@ func (repo PostgresCharacterRepository) preloadStrings(preloads []string) *gorm.
 
 var (
 	characterStringPreloads []string = []string{
-		"DatabaseName.StringValues",
+		"Name.StringValues",
 		"FullName.StringValues",
 		"Description.StringValues",
 		"Title.StringValues",
