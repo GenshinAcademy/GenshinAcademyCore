3e5bdca427aa20d27b13321f5d58ca8e92955482
Order <43047529+aelariane@users.noreply.github.com>
2023-02-25 07:55:12 +0300

    Added docs

--- pkg/genshin_core/errors/errors.go
+++ pkg/genshin_core/errors/errors.go
@@ -13,15 +13,16 @@ func(err characterNotFoundError) Error() string {
     return fmt.Sprintf("Character with id {%s} not found.", err.id)
 }
 
+// CharacterNotFound Indicates that character was not found
 func CharacterNotFound(id models.ModelId) error {
     return characterNotFoundError{
         id: id,
     }
 }
 
-
 type emptyIdError struct{}
 
+// EmptyId Indicates that Id was empty.
 func EmptyId() error {
     return emptyIdError{}
 }
