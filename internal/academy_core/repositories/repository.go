cba0c8fe805575a34130aa3551e7bbe26f4443f3
Jagerente <JustlPains@gmail.com>
2023-02-26 23:47:37 +0700

    comments;

--- internal/academy_core/repositories/repository.go
+++ internal/academy_core/repositories/repository.go
@@ -3,8 +3,8 @@ package repositories
 import (
 	"ga/internal/academy_core/models"
 	"ga/internal/academy_core/repositories/find_parameters"
-    genshin_models "ga/pkg/genshin_core/models"
-    "ga/pkg/genshin_core/models/languages"
+	genshin_models "ga/pkg/genshin_core/models"
+	"ga/pkg/genshin_core/models/languages"
 )
 
 type IRepository interface {
@@ -12,15 +12,19 @@ type IRepository interface {
 }
 
 type ILanguageRepository interface {
+	// AddLanguage adds language to the database
 	AddLanguage(language *models.Language)
+
+	// FindLanguage finds language in database.
+	// If none is found returned ID is 0.
 	FindLanguage(lang languages.Language) models.Language
 }
 
 type ICharacterRepository interface {
 	IRepository
 	GetCharacterIds(parameters find_parameters.CharacterFindParameters) []genshin_models.ModelId
-    FindCharacterById(characterId models.AcademyId) (models.Character, bool)
-    FindCharacterByGenshinId(characterId genshin_models.ModelId) (models.Character, bool)
+	FindCharacterById(characterId models.AcademyId) (models.Character, bool)
+	FindCharacterByGenshinId(characterId genshin_models.ModelId) (models.Character, bool)
 	FindCharacters(parameters find_parameters.CharacterFindParameters) []models.Character
 	AddCharacter(character *models.Character) (models.AcademyId, error)
 	UpdateCharacter(character *models.Character)
