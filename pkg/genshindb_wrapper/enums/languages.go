8a042de1fc5550889b1f28d8818170ea58ad2dc0
Jagerente <JustlPains@gmail.com>
2023-02-27 00:22:00 +0700

    genshin-db wrapper;

---
+++ pkg/genshindb_wrapper/enums/languages.go
@@ -0,0 +1,19 @@
+package enums
+
+type Language string
+
+const (
+	Russian            Language = "Russian"
+	English            Language = "English"
+	ChineseSimplified  Language = "ChineseSimplified"
+	ChineseTraditional Language = "ChineseTraditional"
+	French             Language = "French"
+	German             Language = "German"
+	Indonesian         Language = "Indonesian"
+	Japanese           Language = "Japanese"
+	Korean             Language = "Korean"
+	Portuguese         Language = "Portuguese"
+	Spanish            Language = "Spanish"
+	Thai               Language = "Thai"
+	Vietnamese         Language = "Vietnamese"
+)
