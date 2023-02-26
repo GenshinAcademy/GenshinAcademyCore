38ff62e17ff6b9f70436b5c1ca384b5e676e04a3
Jagerente <JustlPains@gmail.com>
2023-02-26 23:46:27 +0700

    logger; configuration;

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"ga/internal/academy_core"
 	academy_models "ga/internal/academy_core/models"
+	"ga/internal/configuration"
 	academy_postgres "ga/internal/db_postgres/implementation/academy"
 	core "ga/pkg/genshin_core"
-    "ga/pkg/genshin_core/value_objects"
+	"ga/pkg/genshin_core/value_objects"
 
-    "ga/pkg/genshin_core/models"
+	"ga/pkg/genshin_core/models"
 	"ga/pkg/genshin_core/models/enums"
 	"ga/pkg/genshin_core/models/languages"
 	"ga/pkg/genshin_core/repositories"
@@ -46,13 +46,16 @@ func updateCharacter(character *models.Character, repo repositories.CharacterRep
 }
 
 func main() {
+	configuration.Init()
+
+	logger := configuration.Logger
+
 	var dbConfig academy_postgres.PostgresDatabaseConfiguration = academy_postgres.PostgresDatabaseConfiguration{
-		Host:         "localhost",
-		UserName:     "postgres",
-		UserPassword: "12345678",
-		DatabaseName: "gacore_db",
-		Port:         5432,
-		ServerPort:   0,
+		Host:         configuration.ENV.DBHost,
+		UserName:     configuration.ENV.DBUserName,
+		UserPassword: configuration.ENV.DBUserPassword,
+		DatabaseName: configuration.ENV.DBName,
+		Port:         configuration.ENV.DBPort,
 	}
 
 	academy_postgres.InitializePostgresDatabase(dbConfig)
@@ -80,10 +83,12 @@ func main() {
 			LanguageName: string(defaultLanguage),
 		}
 		langRepo.AddLanguage(&language)
-		fmt.Println("Language created successfully!\n", language)
+		logger.Sugar().Infow("Language created successfully!",
+			"language", language)
 		upd = true
 	} else {
-		fmt.Println("Language found soccessfully!\n", language)
+		logger.Sugar().Infow("Language found successfully!",
+			"language", language)
 	}
 
 	//Get provider (with default language, and then character repo)
@@ -95,25 +100,30 @@ func main() {
 	if len(result) == 0 {
 		createHuTao(&characterRepo)
 		var hutaoNew = characterRepo.FindCharacters(findParams)[0]
-		fmt.Println("Hu tao successfully added to DB!\n", hutaoNew)
+		logger.Sugar().Infow("Character successfully added to DB!",
+			"character", hutaoNew)
+
 		return
 	}
 
-    upd = true
-    if len(hutao.Icons) == 0 {
-        hutao.Icons = append(hutao.Icons, value_objects.CharacterIcon{
-            Type: value_objects.Front,
-            Url: "http://localhost/hu_tao.png",
-        })
-    }
+	upd = true
+	if len(hutao.Icons) == 0 {
+		hutao.Icons = append(hutao.Icons, value_objects.CharacterIcon{
+			Type: value_objects.Front,
+			Url:  "http://localhost/hu_tao.png",
+		})
+	}
 
 	if upd {
 		updateCharacter(&hutao, characterRepo)
-		fmt.Println("Hu Tao model updated successfully!\n", hutao)
+		logger.Sugar().Infow("Character updated successfully!",
+			"character", hutao)
 	}
 
-	fmt.Println("Hu Tao model retrieved successfully!\n", hutao)
+	logger.Sugar().Infow("Character model retrieved successfully!",
+		"character", hutao)
 
 	var hutaoacad, _ = gacore.GetDefaultProvider().NewCharacterRepo().FindCharacterByGenshinId("hu_tao")
-	fmt.Println("Hu Tao ACADEMY model retrieved successfully!\n", hutaoacad)
+	logger.Sugar().Infow("Character ACADEMY model retrieved successfully!",
+		"character", hutaoacad)
 }
