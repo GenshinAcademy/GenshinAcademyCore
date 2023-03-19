0d78be913b64112dcc3e4294a92bb665d63d9ef5
Order <43047529+aelariane@users.noreply.github.com>
2023-03-19 12:08:25 +0300

    Update news.go

--- internal/db_postgres/models/news.go
+++ internal/db_postgres/models/news.go
@@ -8,7 +8,7 @@ type News struct {
 	Title         String
 	DescriptionId DBKey `gorm:"column:description"`
 	Description   String
-	PreviewUrl    string    `gorm:"column:preview_url"`
-	RecirectUrl   string    `gorm:"column:redirect_url"`
+	PreviewUrl    string    `gorm:"column:preview_url;type:varchar"`
+	RedirectUrl   string    `gorm:"column:redirect_url;type:varchar"`
 	CreatedAt     time.Time `gorm:"column:created_at"`
 }
