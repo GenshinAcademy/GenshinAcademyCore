6b4c3aebecfc1e5a0b0678b5677a11f04e6b4ac0
Order <43047529+aelariane@users.noreply.github.com>
2023-03-19 12:11:09 +0300

    Created academy news find params

---
+++ internal/academy_core/repositories/find_parameters/news.go
@@ -0,0 +1,23 @@
+package find_parameters
+
+import (
+	"ga/pkg/genshin_core/repositories/find_parameters"
+	"time"
+)
+
+type NewsFindParameters struct {
+	FindParameters
+	SliceOptions         find_parameters.SliceParameters
+	PublishTimeFrom      *time.Time
+	PublishTimeTo        *time.Time
+	SortByDescendingTime bool
+}
+
+func (parameters NewsFindParameters) Slice(offset uint32, limit uint32) NewsFindParameters {
+	parameters.SliceOptions = find_parameters.SliceParameters{
+		Offset: offset,
+		Amount: limit,
+	}
+
+	return parameters
+}
