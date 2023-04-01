00412f6a095dae8cd3c7c669b2871fd7e68d1051
Order <43047529+aelariane@users.noreply.github.com>
2023-04-01 08:19:09 +0300

    Created table find params

---
+++ internal/academy_core/repositories/find_parameters/table.go
@@ -0,0 +1,19 @@
+package find_parameters
+
+import (
+	"ga/pkg/genshin_core/repositories/find_parameters"
+)
+
+type TableFindParameters struct {
+	FindParameters
+	SliceOptions find_parameters.SliceParameters
+}
+
+func (parameters TableFindParameters) Slice(offset uint32, limit uint32) TableFindParameters {
+	parameters.SliceOptions = find_parameters.SliceParameters{
+		Offset: offset,
+		Amount: limit,
+	}
+
+	return parameters
+}
