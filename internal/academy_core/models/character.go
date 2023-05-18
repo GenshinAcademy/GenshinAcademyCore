34065ac59df15a9fb61a34d003e092efbf912ba1
Jagerente <JustlPains@gmail.com>
2023-05-18 21:46:55 +0700

    typo fix;

--- internal/academy_core/models/character.go
+++ internal/academy_core/models/character.go
@@ -1,13 +1,12 @@
 package models
 
 import (
-	artifact_proft "ga/internal/academy_core/value_objects/artifact_profit"
+	artifact_profit "ga/internal/academy_core/value_objects/artifact_profit"
 	"ga/pkg/genshin_core/models"
 )
-
 
 type Character struct {
 	AcademyModel
 	models.Character
-	Profits []artifact_proft.ArtifactProfit
+	Profits []artifact_profit.ArtifactProfit
 }
